74a4763d53ee67b5aca0ad6c506302606fe24dee
Michał Długosz <mdugosz@gmail.com>
2020-12-29 19:49:53 +0100

    modyfied startDrawing function added basic changeColor function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,44 +1,55 @@
 const main = document.querySelector('.grid');
 const clear = document.querySelector('#clear');
 const input = document.querySelector('input');
+const buttons = document.querySelectorAll('button');
+const zielony = document.querySelector('#green');
 
-createGrid(100);
+
+createGrid(50);
 
 function createGrid(size) {
+    main.innerHTML = '';
     const width = main.offsetWidth;
     let dimension = size * size;
     let divSize = width/size;
-
+    
     for (let i = 0; i < dimension; i++) {
         const item = document.createElement('div');
         main.appendChild(item);
     }
     main.style.gridTemplateColumns = `repeat(${size}, ${divSize}px)`;
     main.style.gridTemplateRows = `repeat(${size}, ${divSize}px)`;
+    startDrawing();
 }
 
 
-
-const wybierz = main.querySelectorAll('div');
-
-wybierz.forEach(function (div) {
-    div.addEventListener('mouseenter', function () {
-        div.style.background = 'green';
-    })
-})
-
-function  wyczysc(to) {
-    to.forEach(function (div) {
-        div.style.background = 'black';
+function startDrawing(color) {
+    const blackboard = main.querySelectorAll('div');
+    
+    blackboard.forEach(item => {
+        item.addEventListener('mouseenter', y => {
+            changeColor(item, color);
+        });
     })
 }
 
-clear.addEventListener('click', function() {
-    wyczysc(wybierz);
-});
 
-input.addEventListener('change', function () {
-    wyczysc(wybierz);
-    createGrid(input.value);
-    console.log(input.value);
-})
+
+function changeColor(div, color) {
+       
+    switch(color) {
+        case 'green':
+            div.style.background = 'green';
+            break;
+        case 'red':
+            div.style.background = 'red';
+            break;
+        default:
+            div.style.background = 'yellow';
+            break;
+    }
+}
+
+clear.addEventListener('click', () => {createGrid(input.value)});
+input.addEventListener('change', () => {createGrid(input.value)});
+zielony.addEventListener('click', () => {startDrawing(zielony.value)});
